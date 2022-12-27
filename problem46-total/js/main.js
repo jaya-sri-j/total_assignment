7da9559829f24b286b0c6399f6201d1325484457
jaya-sri-j <jayasrijagan28@gmail.com>
2022-12-27 10:38:56 +0530

    adding files.

---
+++ problem46-total/js/main.js
@@ -0,0 +1,9 @@
+var a=prompt("enter the value of a");
+a=parseInt(a);
+var b=prompt("enter the value of b");
+b=parseInt(b);
+var c=prompt("enter the value of c");
+c=parseInt(c);
+var total;
+var total=(a+b+c);
+alert(total);
